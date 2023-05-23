79f4a9fbd131b337387b7f190c272a1ec0dd2577
Callum McLuskey <callummcluskey100@gmail.com>
2023-05-23 18:07:42 +0100

    added basic sveltekit page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<div class="parent-container">
+	<div class="container">
+		<h1>Agile SaaS tool</h1>
+		<p>Application coming soon!</p>
+	</div>
+</div>
+
+<style lang="scss">
+	:root {
+		font-family: Arial, Helvetica, sans-serif;
+		background-color: rgb(60, 60, 60);
+		color: white;
+	}
+	.parent-container {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		min-height: 100%;
+	}
+	.container {
+		width: clamp(300px, 100%, 800px);
+		height: 100%;
+		padding: 20px;
+
+		& * {
+			margin: 0;
+		}
+	}
+</style>
