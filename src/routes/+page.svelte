cf0f07c0a4382617f092525b8773e66a5095ffde
Callum McLuskey <callummcluskey100@gmail.com>
2023-05-27 16:36:49 +0100

    added very basic styling to page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,83 @@
-<div class="parent-container">
+<script>
+	import "@fontsource/roboto";
+</script>
 	<div class="container">
-		<h1>Agile SaaS tool</h1>
+		<div class="logo"></div>
+		<div>
+		<h1><span>F</span><span>l</span><span>o</span><span>b</span><span>o</span><span>a</span><span>r</span><span>d</span></h1>
 		<p>Application coming soon!</p>
+		</div>
 	</div>
-</div>
+
+
 
 <style lang="scss">
+	@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
 	:root {
-		font-family: Arial, Helvetica, sans-serif;
-		background-color: rgb(60, 60, 60);
+		font-family: 'Roboto', sans-serif;
+		background-color: rgb(196, 196, 196);
 		color: white;
-	}
-	.parent-container {
-		display: flex;
-		justify-content: center;
-		width: 100%;
-		min-height: 100%;
-	}
-	.container {
-		width: clamp(300px, 100%, 800px);
-		height: 100%;
-		padding: 20px;
+		background: linear-gradient(-45deg, #5293ee, red);
+		background-size: 400% 400%;
+		animation: gradient 15s ease infinite;
+		height:100%;
+		margin:0;
 
-		& * {
-			margin: 0;
-		}
+	}
+	h1,p{margin:0;}
+	h1{
+		font-size: 40px;
+		height:50px;
+		display:flex;
+		align-items: center;
+		cursor:default;
+	}
+	@keyframes loadIn{
+		from{opacity: 0;}
+		to{opacity: 1;}
+	}
+	@keyframes loadOut{
+		from{opacity: 1;}
+		to{opacity: 0;}
+	}
+	h1>span{
+
+		animation: loadIn 0.5s ease;
+	}
+	p{
+		font-size:16px;
+	}
+	h1>span:hover{
+
+		animation:loadOut 0.25s ease;
+	}
+
+	.container{
+		display: flex;
+		width:100vw;
+		align-items: center;
+		gap:30px;
+		justify-content: center;
+		transform: scale(1.25);
+		width: 100vw;
+		min-height: 80vh;
+
+	}
+
+	.logo{
+		width:50px;
+		height:65px;
+		background: url(images/Floboard-logo.svg) no-repeat center;
+		background-size:contain
 	}
 </style>
